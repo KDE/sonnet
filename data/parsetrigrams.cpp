c1ba4874861d16171b272d548620ff047bd7c473
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2013-12-24 01:01:49 +0100

    Add trigrams.
    Add the old set of generated trigram files, fix the build of the
    trigrams map (generation of a QHash that is serialized to a special map
    file for fast loading).

---
+++ data/parsetrigrams.cpp
@@ -0,0 +1,66 @@
+/**
+ * parsetrigrams.cpp
+ *
+ * Parse a set of trigram files into a QHash, and serialize to stdout.
+ *
+ * Copyright 2006 Jacob Rideout <kde@jacobrideout.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ */
+
+#include <QtCore/QString>
+#include <QtCore/QTextStream>
+#include <QtCore/QDir>
+#include <QtCore/QFile>
+#include <QtCore/QHash>
+#include <QtCore/QRegExp>
+#include <QtCore/QStringList>
+#include <QtCore/QDebug>
+
+int main(int argc, char **argv)
+{
+    if (argc<2) return 1;
+
+    QFile sout;
+    sout.open(stdout, QIODevice::WriteOnly);
+    QDataStream out(&sout);
+
+    QDir td(argv[1]);
+
+    QHash< QString, QHash<QString,int> > models;
+
+    QStringList files=td.entryList(QDir::Files);
+
+    Q_FOREACH (const QString& fname, files) {
+
+        QFile fin(td.filePath(fname));
+        fin.open(QFile::ReadOnly | QFile::Text);
+        QTextStream stream(&fin);
+        stream.setCodec("UTF-8");
+        QString lname=fname.toLower();
+
+        while (!stream.atEnd())
+        {
+            QString line = stream.readLine();
+            QRegExp rx("(.{3})\\s+(.*)");
+            int pos = rx.indexIn(line);
+            if ( pos != -1 )
+                models[lname][line.left(3)] = rx.cap(2).toInt();
+        }
+    }
+
+    out << models;
+}
