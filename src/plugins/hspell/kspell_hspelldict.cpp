834813f7b0248e6c4e5a3e375f2179fc34b8847b
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2013-12-30 19:14:13 +0100

    I think I deserve at least some credit now

--- src/plugins/hspell/kspell_hspelldict.cpp
+++ src/plugins/hspell/kspell_hspelldict.cpp
@@ -3,6 +3,7 @@
  *
  * Copyright (C)  2003  Zack Rusin <zack@kde.org>
  * Copyright (C)  2005  Mashrab Kuvatov <kmashrab@uni-bremen.de>
+ * Copyright (C)  2013  Martin Sandsmark <martin.sandsmark@kde.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
