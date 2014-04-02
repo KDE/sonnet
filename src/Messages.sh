867bd34094c1aad1b916a379f7e592bab1ff301b
Aurélien Gâteau <agateau@kde.org>
2014-04-02 17:26:40 +0200

    Extract strings from .ui with extract_tr_strings

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
-# Invoke the extractrc script on all .ui, .rc, and .kcfg files in the sources.
-# The results are stored in a pseudo .cpp file to be picked up by xgettext.
-lst=`find . -name \*.rc -o -name \*.ui -o -name \*.kcfg`
-if [ -n "$lst" ] ; then
-    $EXTRACTRC $lst >> rc.cpp
-fi
-
-# If your framework contains tips-of-the-day, call preparetips as well.
-if [ -f "data/tips" ] ; then
-    ( cd data && $PREPARETIPS > ../tips.cpp )
-fi
-
 # Extract strings from all source files.
-# If your framework depends on KI18n, use $XGETTEXT. If it uses Qt translation
-# system, use $EXTRACT_TR_STRINGS.
-$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/sonnet5.pot
+# EXTRACT_TR_STRINGS extracts strings with lupdate and convert them to .pot with
+# lconvert.
+$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml` -o $podir/sonnet5.pot
