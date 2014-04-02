db10ef8860620dd45c9a6b1f85ef52dd305ad6d2
Aurélien Gâteau <agateau@kde.org>
2014-04-02 14:15:16 +0200

    Fix typo in find command

--- src/Messages.sh
+++ src/Messages.sh
@@ -15,4 +15,4 @@ fi
 # Extract strings from all source files.
 # If your framework depends on KI18n, use $XGETTEXT. If it uses Qt translation
 # system, use $EXTRACT_TR_STRINGS.
-$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -name \*.qml` -o $podir/sonnet5.pot
+$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/sonnet5.pot
