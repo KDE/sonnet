47dc1d669f54e7b3dd90b6cc5359acaa92fe58d8
Aurélien Gâteau <agateau@kde.org>
2014-04-17 10:56:09 +0200

    Add _qt suffix to qt-based .po files

--- src/Messages.sh
+++ src/Messages.sh
@@ -3,4 +3,4 @@
 # Extract strings from all source files.
 # EXTRACT_TR_STRINGS extracts strings with lupdate and convert them to .pot with
 # lconvert.
-$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml` -o $podir/sonnet5.pot
+$EXTRACT_TR_STRINGS `find . -name \*.cpp -o -name \*.h -o -name \*.ui -o -name \*.qml` -o $podir/sonnet5_qt.pot
