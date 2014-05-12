c6f25207cbeaed5673803d9838493c53985b76e8
Albert Astals Cid <aacid@kde.org>
2014-05-12 20:37:37 +0200

    Fix lupdate warning
    /home/scripty/prod/git-unstable/frameworks_sonnet/src/ui/spellcheckdecorator.cpp:35: Ignoring definition of undeclared qualified class
    /home/scripty/prod/git-unstable/frameworks_sonnet/src/ui/spellcheckdecorator.cpp:137: Qualifying with unknown namespace/class ::SpellCheckDecorator

--- src/ui/spellcheckdecorator.cpp
+++ src/ui/spellcheckdecorator.cpp
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301  USA
  */
-#include <spellcheckdecorator.h>
+#include "spellcheckdecorator.h"
 
 // Local
 #include <highlighter.h>
