KDE/sonnet

commit 0a96acf251baa5c9dd042d093ab2bf8fcee10502

Use the current hunspell API
Differential Revision: https://phabricator.kde.org/D11193

src/plugins/hunspell/hunspelldict.cpp
@@ -102,9 +102,9 @@ bool HunspellDict::isCorrect(const QString &word) const
     if (!m_speller) {
         return false;
     }
-    int result = m_speller->spell(toDictEncoding(word).constData());
+    bool result = m_speller->spell(toDictEncoding(word).toStdString());
     qCDebug(SONNET_HUNSPELL) << " result :" << result;
-    return result != 0;
+    return result;
 }
 
 QStringList HunspellDict::suggest(const QString &word) const
@@ -112,13 +112,10 @@ QStringList HunspellDict::suggest(const QString &word) const
     if (!m_speller) {
         return QStringList();
     }
-    char **selection;
     QStringList lst;
-    int nbWord = m_speller->suggest(&selection, toDictEncoding(word).constData());
-    for (int i = 0; i < nbWord; ++i) {
-        lst << m_codec->toUnicode(selection[i]);
-    }
-    m_speller->free_list(&selection, nbWord);
+    const auto suggestions = m_speller->suggest(toDictEncoding(word).toStdString());
+    for_each (suggestions.begin(), suggestions.end(), [this, &lst](const std::string &suggestion) {
+            lst << m_codec->toUnicode(suggestion.c_str()); });
     return lst;
 }
 

commit a18b3810a18de85dc5d896a09c2040c52b4a85f5

hunspell: Restore build with hunspell <=v1.5.0
Summary:
Commit 0a96acf251baa5c9dd042d093ab2bf8fcee10502 broke compatibility with
hunspell versions earlier than v1.5.1. v1.5.1 is was released Nov 2016,
thus only two years old.

This patch restores the build with hunspell versions provided on more
conservative platforms (e.g. CentOS 6.8, which provides hunspell v1.2.x)

Reviewers: rjvbb, dfaure, mlaurent

Reviewed By: dfaure

Subscribers: kde-frameworks-devel

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D14987

src/plugins/hunspell/CMakeLists.txt
@@ -4,6 +4,15 @@ set(sonnet_hunspell_PART_SRCS
 )
 ecm_qt_declare_logging_category(sonnet_hunspell_PART_SRCS HEADER hunspelldebug.h IDENTIFIER SONNET_HUNSPELL CATEGORY_NAME sonnet.plugins.hunspell)
 
+# see: https://phabricator.kde.org/R246:0a96acf251baa5c9dd042d093ab2bf8fcee10502
+set(USE_OLD_HUNSPELL_API TRUE)
+if (PKG_HUNSPELL_VERSION GREATER "1.5.0")
+    set(USE_OLD_HUNSPELL_API FALSE) # new API introduced in v1.5.1 (cf. https://github.com/hunspell/hunspell/commit/8006703dafeebce19f2144c5cf180812eb99693a)
+endif()
+message(STATUS "Using old hunspell API: ${USE_OLD_HUNSPELL_API}")
+
+configure_file(config-hunspell.h.cmake ${CMAKE_CURRENT_BINARY_DIR}/config-hunspell.h)
+
 add_library(sonnet_hunspell MODULE ${sonnet_hunspell_PART_SRCS})
 target_include_directories(sonnet_hunspell SYSTEM PUBLIC ${HUNSPELL_INCLUDE_DIRS})
 target_link_libraries(sonnet_hunspell PRIVATE KF5::SonnetCore ${HUNSPELL_LIBRARIES})

src/plugins/hunspell/config-hunspell.h.cmake
@@ -1,0 +1,1 @@
+#cmakedefine01 USE_OLD_HUNSPELL_API

src/plugins/hunspell/hunspelldict.cpp
@@ -18,7 +18,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301  USA
  */
+
 #include "hunspelldict.h"
+
+#include "config-hunspell.h"
 #include "hunspelldebug.h"
 
 #include <QFile>
@@ -102,9 +105,16 @@ bool HunspellDict::isCorrect(const QString &word) const
     if (!m_speller) {
         return false;
     }
+
+#if USE_OLD_HUNSPELL_API
+    int result = m_speller->spell(toDictEncoding(word).constData());
+    qCDebug(SONNET_HUNSPELL) << " result :" << result;
+    return result != 0;
+#else
     bool result = m_speller->spell(toDictEncoding(word).toStdString());
     qCDebug(SONNET_HUNSPELL) << " result :" << result;
     return result;
+#endif
 }
 
 QStringList HunspellDict::suggest(const QString &word) const
@@ -112,10 +122,21 @@ QStringList HunspellDict::suggest(const QString &word) const
     if (!m_speller) {
         return QStringList();
     }
+
     QStringList lst;
+#if USE_OLD_HUNSPELL_API
+    char **selection;
+    int nbWord = m_speller->suggest(&selection, toDictEncoding(word).constData());
+    for (int i = 0; i < nbWord; ++i) {
+        lst << m_codec->toUnicode(selection[i]);
+    }
+    m_speller->free_list(&selection, nbWord);
+#else
     const auto suggestions = m_speller->suggest(toDictEncoding(word).toStdString());
     for_each (suggestions.begin(), suggestions.end(), [this, &lst](const std::string &suggestion) {
             lst << m_codec->toUnicode(suggestion.c_str()); });
+#endif
+
     return lst;
 }
 