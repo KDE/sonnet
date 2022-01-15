a2276ebc436fe8be669a47f827a6c95154d20c26
Laurent Montel <montel@kde.org>
2022-01-15 20:07:38 +0100

    Missing "/"

--- src/core/loader.cpp
+++ src/core/loader.cpp
@@ -272,7 +272,7 @@ void Loader::loadPlugins()
 {
 #ifndef SONNET_STATIC
     const QStringList libPaths = QCoreApplication::libraryPaths() << QStringLiteral(INSTALLATION_PLUGIN_PATH);
-    const QString pathSuffix(QStringLiteral("kf" QT_STRINGIFY(QT_VERSION_MAJOR)) + QStringLiteral("/sonnet/"));
+    const QString pathSuffix(QStringLiteral("/kf" QT_STRINGIFY(QT_VERSION_MAJOR)) + QStringLiteral("/sonnet/"));
     for (const QString &libPath : libPaths) {
         QDir dir(libPath + pathSuffix);
         if (!dir.exists()) {
